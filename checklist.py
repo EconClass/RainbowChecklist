49dbbdbb4058d6a532a5b75289562893524b2afe
EconC1ass <bizmail327@gmail.com>
2018-08-28 13:26:15 -0700

    Rework test function

--- checklist.py
+++ checklist.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Initial Conditions
 checklist = list()
-# checklist.append("Hello")
-# checklist.append("World")
 
 # Create Items
 def create(item):
@@ -33,6 +31,13 @@ def mark_completed(index):
     mark_item = checklist[index]
     print(str('√') + "{}".format(mark_item))
 
+# Prompt a user input
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = raw_input(prompt)
+    return user_input
+
 # Select functions to run
 def select(function_code):
     # Create item
@@ -44,38 +49,35 @@ def select(function_code):
     elif function_code == "R":
         item_index = input("Index Number? ")
         # Remember that item_index must actually exist or our program will crash.
-        read(item_index)
+        print(read(item_index))
 
     # Print all items
     elif function_code == "P":
         list_all_items()
 
+    # Stop the loop
+    elif function_code == "Q":
+        return False
+
     # Catch all
     else:
         print("Unknown Option")
 
-# Prompt a user input
-def user_input(prompt):
-    # the input function will display a message in the terminal
-    # and wait for user input.
-    user_input = raw_input(prompt)
-    return user_input
-
 # Test Functions
 def test():
-    create("purple sox")
-    create("red cloak")
-    print(read(0))
-    print(read(1))
-    update(0, "purple socks")
-    destroy(1)
-    print(read(0))
-    list_all_items()
-    mark_completed(0)
-    # select("C")
-    # list_all_items()
-    # select("R")
-    # select("P")
-    select("X")
 
+
+# Clear Terminal
+def clear_terminal():
+    import os
+    os.system("clear")
+
+# Run the test function
 test()
+clear_terminal()
+
+running = True
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list, P to display list, and Q to quit: ")
+    running = select(selection)
